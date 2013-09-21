ab018a251cab3e90868ed2198eb076375c1fbd72
U-LAPTOP\Jonathan Eidelman <Jonathan Eidelman@LAPTOP.(none)>
2013-09-21 09:25:09 -0500

    Calculadora con todas sus funcionalidades

---
+++ parser.C
@@ -0,0 +1,123 @@
+#include "parser.h"
+#include "calcex.h"
+#include <string>
+#include <sstream>
+
+Parser::Parser(istream* in) {
+   scan = new Scanner(in);
+}
+
+Parser::~Parser() {
+   try {
+      delete scan;
+   } catch (...) {}
+}
+
+AST* Parser::parse() {
+   return Prog();
+}
+
+AST* Parser::Prog() {
+   AST* result = Expr();
+   Token* t = scan->getToken();
+
+   if (t->getType() != eof) {
+      cout << "Syntax Error: Expected EOF, found token at column " << t->getCol() << endl;
+      throw ParseError;
+   }
+
+   return result;
+}
+
+AST* Parser::Expr() { 
+  return RestExpr(Term());
+}
+
+AST* Parser::RestExpr(AST* e) {
+   Token* t = scan->getToken();
+
+   if (t->getType() == add) {
+      return RestExpr(new AddNode(e,Term()));
+   }
+
+   if (t->getType() == sub)
+      return RestExpr(new SubNode(e,Term()));
+
+   scan->putBackToken();
+
+   return e;
+}
+
+AST* Parser::Term() {
+   //write your Term() code here. This code is just temporary
+   //so you can try the calculator out before finishing it.
+   //Token* t = scan->getToken();
+   //cout << "Term not implemented" << endl;
+   //throw ParseError;
+  return RestTerm(Storable());
+}
+
+AST* Parser::RestTerm(AST* e) {
+  //cout << "RestTerm not implemented" << endl;
+   //throw ParseError;
+   Token* t = scan->getToken();
+
+   if (t->getType() == times) {
+     return RestTerm(new MulNode(e,Storable()));
+   }
+
+   if (t->getType() == divide)
+      return RestTerm(new DivNode(e,Storable()));
+
+   scan->putBackToken();
+
+   return e;
+}
+
+AST* Parser::Storable() {
+  AST* factor = Factor();
+  AST* ret;
+  Token* t = scan->getToken();
+  if (t->getType() == keyword){
+    if(t->getLex() == "S" || t->getLex() == "s"){
+      ret = new StoreNode(factor);
+    }
+    else {
+      cout << "Syntax Error: Expected S or s, found token at column " << t->getCol() << endl;
+      throw ParseError;
+    }
+  }
+  else {
+    scan->putBackToken();
+    ret = factor;
+  }
+  return ret;
+}
+
+AST* Parser::Factor() {
+  AST* ret;
+  Token* t = scan->getToken();;
+  if (t->getType() == number) {
+      istringstream in(t->getLex());
+      int val;
+      in >> val;
+      return new NumNode(val);
+  }else if(t->getType() == keyword){
+    if(t->getLex() == "R" || t->getLex() == "r"){
+      ret = new RecallNode();
+    }
+    else {
+      cout << "Syntax Error: Expected R or r, found token at column " << t->getCol() << endl;
+      throw ParseError;
+    }
+  }
+  else if (t->getType() == lparen){
+    ret = Expr();
+    t = scan->getToken();
+    if (t->getType() != rparen){
+      cout << "Syntax Error: Expected ), found token at column " << t->getCol() << endl;
+    }
+  }
+  return ret; 
+}
+   
