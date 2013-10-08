3e67b7109a026cfe6c351262895de68739e6a371
maestro252 <eidelmanjonathan@hotmail.com>
2013-10-08 17:11:31 -0500

    Se agrega la funcionalidad -i, interactiva

--- calc.C
+++ calc.C
@@ -10,7 +10,28 @@ Calculator* calc;
 
 int main(int argc, char* argv[]) {
    string line;
- 
+   string arg;
+   string EOF = "";
+   if(argc > 1){
+     arg = argv[1];
+   }
+   if (arg == "-i"){
+     try {
+       cout << "> ";
+       while(getline(cin, line)){
+	 //cout << "> " ;
+	 if(line == EOF)return 0;
+	 calc = new Calculator();
+	 int result = calc->eval(line);
+	 cout << "= " << result << endl;
+	 cout << "> ";
+	 delete calc;
+       }
+       return 0;
+     }catch(Exception e){
+       cout << "Program aborted due to exception!" << endl;
+     }
+   }else{
    try {
 
       cout << "Please enter a calculator expression: ";
@@ -29,6 +50,7 @@ int main(int argc, char* argv[]) {
    }
    catch(Exception ex) {
       cout << "Program Aborted due to exception!" << endl;
+   }
    }
 }
    
